42a71c8651e464f0ff5402e7c83f87856325367b
nyo <nyo4rdace@gmail.com>
2020-09-12 01:27:40 +0800

    add type of variables

--- web-server/terraform.tfvars
+++ web-server/terraform.tfvars
@@ -1,4 +1,6 @@
 cidr            = "10.0.0.0/16"
 public_subnet   = "10.0.1.0/24"
 public_key_path = "~/.ssh/id_rsa.pub"
-ami             = "ami-0461b11e2fad8c14a"
+
+# Ubuntu Server 20.04 LTS (HVM), SSD Volume Type, 64-bit x86
+ami = "ami-0461b11e2fad8c14a"
--- web-server/variables.tf
+++ web-server/variables.tf
@@ -1,25 +1,31 @@
 variable "aws_region" {
+  type        = string
   description = "AWS region to launch servers."
   default     = "ap-northeast-1"
 }
 
 variable "cidr" {
+  type        = string
   description = "vpc cidr block"
 }
 
 variable "public_subnet" {
+  type        = string
   description = "public subnet cidr block"
 }
 
 variable "public_key_path" {
+  type        = string
   description = "Path to SSH public key"
   default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "ami" {
-  description = "ami id, default as Ubuntu Server 20.04 LTS (HVM), SSD Volume Type, 64-bit x86"
+  type        = string
+  description = "ami id"
 }
 
 variable "my_ip" {
+  type        = string
   description = "my ip to allow ssh connection"
 }
